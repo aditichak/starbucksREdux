931a57ed5aadbd65e0b47e834c06605fc4ab57f0
nanstc <nanstchen@gmail.com>
2016-10-23 20:47:37 -0700

    adj list now has proper adjs, although question does not give proper answer yet

--- function.pl
+++ function.pl
@@ -1,24 +1,13 @@
 
-% noun_phrase(T0,T4,Ind,C0,C4) is true if
-%  T0 and T4 are list of words, such that
-%        T4 is an ending of T0
-%        the words in T0 before T4 (written T0-T4) form a noun phrase
-%  Ind is the individual that the noun phrase is referring to
-%  C0 and C4 are lists of relations such that
-%        C0 is an ending of C4 and
-%        the relations in C4-C0 give the constraints on Ind implied by the noun phrase
-% A noun phrase is a determiner followed by adjectives followed
-% by a noun followed by an optional modifying phrase:
-noun_phrase(T0,T5,Ind,C0,C5,L1,L2) :-
+noun_phrase(T0,T5,Ind,C0,C5,L0,L2) :-
     prep(T0,T1,Ind,C0,C1),
     det(T1,T2,Ind,C1,C2),
-    adjectives(T2,T3,Ind,C2,C3,L1,L2),
+    adjectives(T2,T3,Ind,C2,C3,L0,L1),
     noun(T3,T4,Ind,C3,C4),
     mp(T4,T5,Ind,C4,C5,L1,L2).
 
 
-% Determiners (articles) are ignored in this oversimplified example.
-% They do not provide any extra constaints.
+% Determiners do not provide any extra constaints.
 det([the | T],T,_,C,C).
 det([a | T],T,_,C,C).
 det(T,T,_,C,C).
@@ -27,12 +16,11 @@ det(T,T,_,C,C).
 prep([in | T],T,_,C,C).
 prep(T,T,_,C,C).
 
-% Adjectives consist of a sequence of adjectives.
-% The meaning of the arguments is the same as for noun_phrase
+% Adjectives 
 adjectives(T0,T2,Ind,C0,C2,L0,L2) :-
     adj(T0,T1,C0,C1,L0,L1),
     adjectives(T1,T2,Ind,C1,C2,L1,L2).
-adjectives(T,T,_,C,C,_,_).
+adjectives(T,T,_,C,C,L,L).
 
 % mp for '...that' and 'how many ... are'
 mp(T0,T2,I1,C0,C2,L0,L2) :-
@@ -41,17 +29,18 @@ mp(T0,T2,I1,C0,C2,L0,L2) :-
 mp([that|T0],T2,I1,C0,C2,L0,L2) :-
     reln(T0,T1,I1,I2,C0,C1,L0,L1),
     noun_phrase(T1,T2,I2,C1,C2,L1,L2).
-mp(T,T,_,C,C,_,_).
+mp(T,T,_,C,C,L,L).
 
 % reln(T0,T1,I1,I2,R0,R1,L0,L2) 
 %reln([calories | T],T,I1,I2,C,[calories_in(I1,I2,I3,I4,I5)|C],_,_).
-reln(T,T,_,_,R,R,_,_).
+reln(T,T,_,_,R,R,L,L).
 
 % DICTIONARY
 
-% adj(T0,T1,Ind,C0,C1) is true if T0-T1 is an adjective that provides properties C1-C0 to Ind
-adj([Adj| T],T,C,[size(Adj)|C],L,[L|Adj]). % append size
-adj([Adj| T],T,C,[milk(Adj)|C],L,[L|Adj]). % append milk
+% adj(T0,T1,Ind,C0,C1)
+adj([Adj| T],T,C,[size(Adj)|C],L,[Adj|L]):-size(Adj). 
+adj([Adj| T],T,C,[milk(Adj)|C],L,[Adj|L]):-milk(Adj).
+adj([Adj| T],T,C,C,L,L):-drink(Adj). 
 % adj([Adj| T],T,C,[whip(Adj)|C],L,[L|true]) :- whip(Adj). 
 % adj([Adj| T],T,C,[whip(Adj)|C],L,[L|false]) :- \+whip(Adj).
 
@@ -79,20 +68,10 @@ question([how, much | T0],T2,Ind,C0,C2,L0,L2) :-
     noun_phrase(T0,[is|T1],Ind,C0,C1,L0,L1),
     mp(T1,T2,Ind,C1,C2,L1,L2).
 
-question([is | T0],T2,Ind,C0,C2,L0,L2) :-
-    noun_phrase(T0,T1,Ind,C0,C1,L0,L1),
-    mp(T1,T2,Ind,C1,C2,L1,L2).
-
 % "how many calories are in a tall pumpkin spice latte"
-question([how, many, calories| T0],T2,Cal,C0,C2,L0,L2) :-  
-    noun_phrase(T0,T1,Ind,C0,C1,L0,L1),
-    mp(T1,T2,Ind,C1,C2,L1,L2),
-    calories(Ind,L2,Cal).
-
-question([are | T0],T2,Ind,C0,C2,L0,L2) :-
+question([how, many, calories| T0],T2,Ind,C0,C2,L0,L2) :-  
     noun_phrase(T0,T1,Ind,C0,C1,L0,L1),
     mp(T1,T2,Ind,C1,C2,L1,L2).
-
 
 question([what | T0],T2,Ind,C0,C2,L0,L2) :-
     noun_phrase(T0,T1,Ind,C0,C1,L0,L1),
@@ -101,8 +80,9 @@ question([what | T0],T2,Ind,C0,C2,L0,L2) :-
 
 % ask(Q,A) gives answer A to question Q
 ask(Q,A) :-
-    question(Q,[],A,[],C,[],L),
-    prove_all(C).
+    question(Q,[],Ind,[],C,[],L),
+    prove_all(C),
+    calories(Ind,L,A).
 
 % prove_all(L) proves all elements of L against the database
 prove_all([]).
